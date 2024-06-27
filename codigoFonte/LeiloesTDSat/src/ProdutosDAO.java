e0cf8c8fe964207cd21bbacb2e0da4b32a2bee5e
luginheski <rodrigo514_2@hotmail.com>
2024-06-27 19:51:54 -0300

    crianção do comando salvar

--- codigoFonte/LeiloesTDSat/src/ProdutosDAO.java
+++ codigoFonte/LeiloesTDSat/src/ProdutosDAO.java
@@ -1,8 +1,8 @@
 
-import java.sql.PreparedStatement;
 import java.sql.Connection;
-import javax.swing.JOptionPane;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ProdutosDAO {
@@ -12,9 +12,21 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
 
-    public void cadastrarProduto(ProdutosDTO produto) {
+    public int cadastrarProduto(ProdutosDTO produto) {
 
-        //conn = new conectaDAO().connectDB();
+        conn = new conectaDAO().connectDB();
+        int status;
+        try{
+            prep = conn.prepareStatement("INSERT INTO produtos VALUES(?,?,?)");
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            status = prep.executeUpdate();
+            return status;
+        }catch(SQLException ex){
+            System.out.println("Erro ao conectar" + ex.getMessage());
+            return ex.getErrorCode();
+        }
     }
 
     public ArrayList<ProdutosDTO> listarProdutos() {
--- codigoFonte/LeiloesTDSat/src/conectaDAO.java
+++ codigoFonte/LeiloesTDSat/src/conectaDAO.java
@@ -9,7 +9,6 @@ import javax.swing.JOptionPane;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 public class conectaDAO {
 
     public Connection connectDB() {
@@ -17,10 +16,7 @@ public class conectaDAO {
 
         try {
 
-
             conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=lorenteamo");
-
-
 
         } catch (SQLException erro) {
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
