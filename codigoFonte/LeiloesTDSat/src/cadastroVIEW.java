db309fd8bac6380b09b9d067f9306ab913184766
luginheski <rodrigo514_2@hotmail.com>
2024-06-27 16:04:10 -0300

    Revert "limpeza comentario"
    This reverts commit f62086b33370fa0457e9f6d8ba635c8160bf934c.

--- codigoFonte/LeiloesTDSat/src/cadastroVIEW.java
+++ codigoFonte/LeiloesTDSat/src/cadastroVIEW.java
@@ -1,4 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
 
+/**
+ *
+ * @author Adm
+ */
 public class cadastroVIEW extends javax.swing.JFrame {
 
     /**
